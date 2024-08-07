08531e90196bd08607ffad472109560326771171
XvKuoMing <kamasalyamov@gmail.com>
2024-08-07 11:53:00 +0300

    make it pretty

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
-import asyncio
 import logging
 from aiohttp import web
-from aiogram import flags
-from aiogram.filters import Command
-from aiogram.types import Message
 from aiogram.utils.chat_action import ChatActionMiddleware
 from middlewares.throttling import ThrottlingMiddleware
 from aiogram.webhook.aiohttp_server import setup_application
 
+from routers.generator import generator
 from config.bot_config import tgpt
 from config.dp_config import dp
 from config.webhook_config import (set_webhook,
@@ -17,26 +14,15 @@ from config.webhook_config import (set_webhook,
                                    SERVER_ADDRESS,
                                    SERVER_PORT
                                    )
-from utils.generation import generate_answer
-from routers.generator import generator
 
-@dp.message(Command('start'))
-@flags.chat_action(initial_sleep=1, action="typing", interval=3)
-async def start_session(message: Message) -> None:
-    name = message.from_user.full_name
-    text = await generate_answer(
-        prompt=f'Поприветствуй пользователя с именем {name}. Расскажи ему пару слов о себе и что ты умеешь.'
-    )
-    # text = text.format(name=name)
-    await message.answer(text=text)
 
 def main():
-    # await bot.delete_webhook(drop_pending_updates=True)
-    dp.message.middleware(ChatActionMiddleware())
-    dp.message.middleware(ThrottlingMiddleware())
+
+    # include router's middlewares
     generator.message.middleware(ChatActionMiddleware())
     generator.message.middleware(ThrottlingMiddleware())
 
+    # include router's to dispatcher
     dp.include_router(generator)
 
     # web logic
@@ -47,8 +33,6 @@ def main():
     setup_application(app, dp, bot=tgpt)
     web.run_app(app, host=SERVER_ADDRESS, port=SERVER_PORT)
 
-    # await dp.start_polling(bot)
-
 
 # when using docker compose, logs would be handled by `docker compose logs -f -&> docker.logs &` command
 logging.basicConfig(
@@ -57,6 +41,3 @@ logging.basicConfig(
     level=logging.INFO
 )
 main()
-# updating docker container https://www.quora.com/How-do-you-update-code-in-a-docker-container
-# https://docs.docker.com/build/ci/
-# asyncio.run(main())
--- routers/generator.py
+++ routers/generator.py
@@ -9,12 +9,18 @@ from utils.generation import generate_answer
 from utils.stt import voice_file_id2text
 from typing import Optional
 
-generator = Router()
+generator = Router()  # handles every logic about text generation
 
 
 @generator.message(~F.text.startswith('/') & F.text)
 @flags.chat_action(initial_sleep=1, action="typing", interval=3)
 async def proceed_dialog(message: Message, text: Optional[str] = None) -> None:
+    """
+    The main generation handler, generates texts, saves dialog history
+    :param message: message to reply
+    :param text: text to generate from, if not provided message.text is used
+    :return: replies to message
+    """
     storage_key = StorageKey(bot_id=tgpt.id,
                              user_id=message.from_user.id,
                              chat_id=message.chat.id)
@@ -35,15 +41,42 @@ async def proceed_dialog(message: Message, text: Optional[str] = None) -> None:
     await dp.storage.update_data(key=storage_key,
                                  data={'history': history})
 
+"""-------------------------------------------special generation cases-------------------------------------------"""
+@generator.message(Command('start'))
+@flags.chat_action(initial_sleep=1, action="typing", interval=3)
+async def start_session(message: Message) -> None:
+    """
+    generate greeting message from manually provided prompt
+    :param message:
+    :return: replies to user with generated message
+    """
+    name = message.from_user.full_name
+    text = f"""Поприветствуй пользователя с именем {name}. 
+    Расскажи ему пару слов о себе и что ты умеешь. 
+    Если ты его уже приветствовал, то спроси, чем ему помочь."""
+    await proceed_dialog(message=message, text=text)
+
 @generator.message(F.voice)
 @flags.chat_action(initial_sleep=1, action="typing", interval=3)
 async def voice2text(message: Message) -> None:
+    """
+    recognises text from voice message and generates text based on the text
+    :param message: message that contains voice message
+    :return: replies to user with generated text
+    """
     voice_file_id = message.voice.file_id
     text = await voice_file_id2text(voice_file_id)
     await proceed_dialog(message=message, text=text)
 
+
+"""-------------------------------------memory management----------------------------------------------"""
 @generator.message(Command('clear'))
 async def clear_chat_history(message: Message) -> None:
+    """
+    allowing user to manually clear dialog history
+    :param message: message from user that implies need of history clearing
+    :return: success message to user
+    """
     storage_key = StorageKey(bot_id=tgpt.id,
                              user_id=message.from_user.id,
                              chat_id=message.chat.id)
