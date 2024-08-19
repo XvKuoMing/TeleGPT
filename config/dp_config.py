eb1f2d103cac0b3a3223b006b4bb641afaaf666e
XvKuoMing <kamasalyamov@gmail.com>
2024-08-19 14:10:15 +0300

    redis conn tester

--- config/dp_config.py
+++ config/dp_config.py
@@ -1,19 +1,25 @@
 import datetime
-import redis.asyncio as redis
+import redis
+import redis.asyncio as aioredis
 from redis.exceptions import ConnectionError
 from config.bot_config import tgpt
 from aiogram import Dispatcher, F
 from aiogram.fsm.storage.redis import RedisStorage
 
 
-# the idea is to use redis ttl cache in order to prevent long caching of unused data
-redis_pool = redis.ConnectionPool(host="127.0.0.1",
-                                  port=6379,
-                                  socket_timeout=1)
-redis_client = redis.client.Redis(connection_pool=redis_pool)
-
 try:
-    await redis_client.ping()
+    redis_host_params = {
+        "host": "127.0.0.1",
+        "port": 6379,
+        "socket_timeout": 1,
+    }
+    # test connection
+    conn = redis.client.Redis(**redis_host_params)
+    conn.ping()
+    conn.close()
+    # production connection
+    redis_pool = aioredis.ConnectionPool(**redis_host_params)
+    redis_client = aioredis.client.Redis(connection_pool=redis_pool)
     session_rate_limit = datetime.timedelta(minutes=10)
     storage = RedisStorage(
         redis=redis_client,
