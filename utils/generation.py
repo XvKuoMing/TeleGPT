89d3dd5ac9e7202f2acc9b962ef02f3690618b3b
XvKuoMing <kamasalyamov@gmail.com>
2024-08-19 11:47:13 +0300

    patch response logic

--- utils/generation.py
+++ utils/generation.py
@@ -1,5 +1,8 @@
 from config.openai_config import BASE_SYSTEM_PROMPT, MODEL, client
 from typing import Optional, List
+import re
+
+comments = re.compile(r"\*.*\*")
 
 async def generate_answer(prompt: str, system: dict = BASE_SYSTEM_PROMPT, history: Optional[List[dict]] = None):
     system = {'role': 'system', 'content': system}
@@ -16,4 +19,11 @@ async def generate_answer(prompt: str, system: dict = BASE_SYSTEM_PROMPT, histor
             }
         ]
     )
-    return res.choices[0].message.content
+    if res is None:
+        return "К сожалению бот пока не может ответить"
+    text = res.choices[0].message.content
+    if (text[0] + text[-1]) == "**":
+        text = text[1:-1]
+    else:
+        text = comments.sub("", text)
+    return text
