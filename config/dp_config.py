c74eb898a587f8731905b6ba5760e591d79aafcd
XvKuoMing <kamasalyamov@gmail.com>
2024-08-19 12:40:34 +0300

    fix redis connection tester

--- config/dp_config.py
+++ config/dp_config.py
@@ -3,24 +3,26 @@ import redis.asyncio as redis
 from redis.exceptions import ConnectionError
 from config.bot_config import tgpt
 from aiogram import Dispatcher, F
-from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.fsm.storage.redis import RedisStorage
 
 
-# try:
-#     # the idea is to use redis ttl cache in order to prevent long caching of unused data
-#     redis_pool = redis.ConnectionPool(host="127.0.0.1",
-#                                       port=6379,
-#                                       socket_timeout=1)  # https://habr.com/ru/companies/sberbank/articles/736464/
-#     redis_client = redis.client.Redis(connection_pool=redis_pool)  # https://redis-py.readthedocs.io/en/stable/connections.html
-#     session_rate_limit = datetime.timedelta(minutes=10)
-#     storage = RedisStorage(
-#         redis=redis_client,
-#         state_ttl=session_rate_limit,
-#         data_ttl=session_rate_limit
-#     )
-# except ConnectionError:
-storage = MemoryStorage()
+# the idea is to use redis ttl cache in order to prevent long caching of unused data
+redis_pool = redis.ConnectionPool(host="127.0.0.1",
+                                  port=6379,
+                                  socket_timeout=1)
+redis_client = redis.client.Redis(connection_pool=redis_pool)
+
+try:
+    redis_client.ping()
+    session_rate_limit = datetime.timedelta(minutes=10)
+    storage = RedisStorage(
+        redis=redis_client,
+        state_ttl=session_rate_limit,
+        data_ttl=session_rate_limit
+    )
+except ConnectionError:
+    from aiogram.fsm.storage.memory import MemoryStorage
+    storage = MemoryStorage()
 
 dp = Dispatcher(bot=tgpt, storage=storage)
 dp.message.filter(F.chat.type == "private")
