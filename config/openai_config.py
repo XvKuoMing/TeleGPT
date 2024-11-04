107394069e70070683643c28aed14dc833d21ad4
XvKuoMing <kamasalyamov@gmail.com>
2024-11-04 17:20:44 +0300

    add prompts

--- config/openai_config.py
+++ config/openai_config.py
@@ -6,13 +6,21 @@ load_dotenv()  # load .env vars
 API_KEY = os.getenv('OPENAI_TOKEN')
 OPENAI_BASE_URL = os.getenv('OPENAI_BASE_URL')
 MODEL = os.getenv('MODEL')
-BASE_SYSTEM_PROMPT = open("config/system.txt", 'r', encoding='utf8').read().replace('\n', ' ')
 
+# Read the base system prompt
+with open("config/system.txt", 'r', encoding='utf8') as base_file:
+    BASE_SYSTEM_PROMPT = base_file.read().replace('\n', ' ')
+
+# Initialize an empty dictionary for system prompts
 SYSTEM_PROMPTS = {}
-for prompt in os.listdir("/prompts/"):
-    name = prompt.split(".")[0]
-    prompt = open("/prompts/"+prompt, encoding="utf-8").read().replace("\n", " ")
-    SYSTEM_PROMPTS[name] = prompt
+
+# Iterate over prompt files in the specified directory
+PROMPTS_DIR = "/prompts/"
+for prompt_file in os.listdir(PROMPTS_DIR):
+    name = prompt_file.split(".")[0]
+    with open(os.path.join(PROMPTS_DIR, prompt_file), encoding="utf-8") as prompt:
+        prompt_content = prompt.read().replace("\n", " ")
+        SYSTEM_PROMPTS[name] = prompt_content
 
 client = AsyncOpenAI(
     api_key=API_KEY,
