080001dbec91eed32f271b10340d40d32cdd2349
XvKuoMing <kamasalyamov@gmail.com>
2024-11-30 13:09:23 +0300

    patch payment 3

--- config/webhook_config.py
+++ config/webhook_config.py
@@ -19,7 +19,7 @@ webhook_handler = SimpleRequestHandler(dispatcher=dp,
 async def set_webhook(bot: Bot):
     await bot.set_webhook(f"{WEBHOOK_ADDRESS}{WEBHOOK_PATH}",
                            certificate=FSInputFile(SELF_SIGNED_CERTIFICATE),
-                           allowed_updates=["message", "callback_query"],
+                           allowed_updates=["message", "callback_query", "pre_checkout_query"],
                            drop_pending_updates=True) # look at this issue: https://github.com/aiogram/aiogram/discussions/1239
 
 async def delete_webhook(bot: Bot):
